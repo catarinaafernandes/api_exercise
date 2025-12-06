4a9837343e76fcbd51cf4aa8093ec5a74bfdde58
catarina <catarinafernandes280@gmail.com>
2025-12-06 19:43:57 +0000

    add and pass test for rent operations(made in parallel to rents_service)

---
+++ tests/test_rent_manual.py
@@ -0,0 +1,20 @@
+from bloqit_api.services.rents_service import dropoff, retrieve, confirm_dropoff, create_rent
+from bloqit_api.schemas.rents import RentSize
+
+rent = create_rent(weight = 5, size = RentSize.M)
+print("Rent created:", rent)
+
+
+#rent = dropoff(rent.id, "6b33b2d1-af38-4b60-a3c5-53a69f70a351")
+rent = dropoff(rent.id, "3c881050-54bb-48bb-9d2c-f221d10f876b")
+print("dropped:", rent)
+
+
+rent = confirm_dropoff(rent.id)
+print("waiting pickup", rent)
+
+
+rent = retrieve(rent.id)
+print("delivered:", rent)
+
+#TODO: test passed, reset jsons - need to track modifications to logfiles      
